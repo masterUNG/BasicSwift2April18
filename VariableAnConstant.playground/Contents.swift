2e520697014ea117a0765911711f516101bbb1d4
MasterUNG <phrombutr@gmail.com>
2018-05-02 06:51:58 +0700

    Variable Finish

---
+++ VariableAnConstant.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//การประกาศตัวแปร ที่สามารถแก้ไขค่าได้
+var nameString = "MasterUNg"    // การประกาศตัวแปรแบบ อักษร String
+var moneyDouble = 40000.50
+var childrenInt = 5
+var statusBool = true
+
+//ลองแก้ไขค่า
+nameString = "Doramon"
+moneyDouble = 50000
+childrenInt = 10
+statusBool = false
+
+//ค่าคงที่ หรือ ตัวแปร ที่ไม่สามารถแก้ไขค่าได้
+var surNameString = "Phrombutr"
+var pi = 3.1416
+
+surNameString = "newPrombutr"
+pi = 123.456
+
+
+
+
+
+
