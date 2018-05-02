fd1f8dd8df489510b57b2dbd54717c38051280ab
MasterUNG <phrombutr@gmail.com>
2018-05-02 08:30:47 +0700

    Explicit Variable and Print on console

---
+++ ExplicitAndPrintOnConsole.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var nameString: String = "MasterUNG"
+var numberIng: Int = 12
+var moneyDouble: Double = 123.22
+var statusBool: Bool = false
+
+print("Hello")
+print("Name = \(nameString) In Thailand \(moneyDouble)")
+
+print("Value of 10% of money ==> \(0.1*moneyDouble)")
+
+
+
+
+
